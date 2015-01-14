05149a014a4d277592e8cd72ff5ae284923d9b7a
krishsorenseen <kristoffer--s@hotmail.com>
2015-01-14 20:17:09 +0100

    hello

---
+++ ToDoList/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Kristoffer Sørensen on 1/14/15.
+//  Copyright (c) 2015 no.sorkri.nith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var imageView: UIView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+        
+        
+    }
+
+
+}
+
